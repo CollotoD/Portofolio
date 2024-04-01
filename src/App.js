dbc6b325a77661a0111254b1544b40fd5b8ba141
Daniel Nogueira <131224382+CollotoD@users.noreply.github.com>
2024-04-01 16:02:42 +0100

    GitCard (#1)
    * Added cards dinamically, gave it some styling in order to have a little bit of a pallette going
    
    * fixed some css issues in order to look better
    
    * Added blinker font

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import GitCard from './components/GitCard'
 
@@ -16,8 +15,8 @@ function App() {
     });
 
     const data = await res.json();
-    console.log(data.map(({ id, full_name, html_url, stargazers_count, watchers_count, updated_at }) => 
-    ({id, full_name, html_url, stargazers_count, watchers_count, updated_at})))
+    setRepos(data.map(({ id, full_name, description, html_url, stargazers_count, watchers_count, updated_at }) => 
+    ({id, full_name, description, html_url, stargazers_count, watchers_count, updated_at})))
   }
 
   const [repos, setRepos] = useState([]);
@@ -43,14 +42,15 @@ function App() {
         </div>
       </section>
       <section className="projects">
+        <h2>PROJECTS</h2>
         <div className="cardHolder">
           {repos?.length > 0
           ?(
-            <div className="container">
+            <>
             {repos.map((repo) => (
-              <GitCard repo={repo}/>
+              <GitCard key={repo.id} repo={repo}/>
             ))}
-            </div>
+            </>
             ) : (
               <div className="empty">
               <h2>No repos found</h2>
--- src/components/GitCard.jsx
+++ src/components/GitCard.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import './css/GitCard.css'
+import React from 'react';
+import './css/GitCard.css';
+import logo from '../collotod_remix.png';
 
-function GitCard () {
+function GitCard({ repo }) {
     return (
-        <>
         <div className="gitCard">
-            <div className="header">
-
-            </div>
-            <div className="details">
-
-            </div>
+            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                <div className="header">
+                    <img src={logo || "https://via.placeholder.com/250"} alt="Repository Logo" />
+                    <h3>{repo.full_name.split("/").slice(1).join("/")}</h3>
+                </div>
+            </a>
+                <div className="details">
+                    <h3>{repo.description || "No description available"}</h3>
+                </div>
+                <div className="stats">
+                    <div className="stargazer">
+                        <h3>{repo.stargazers_count}</h3>
+                        <h3>Stargazers</h3>
+                    </div>
+                    <div className="stargazer">
+                        <h3>{repo.watchers_count}</h3>
+                        <h3>Watchers</h3>
+                    </div>
+                </div> 
         </div>
-        </>
     );
 }
 
-export default GitCard
+export default GitCard;
