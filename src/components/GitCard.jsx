84d5ee78a38b1c0847af58c295516e09f0831e9e
CollotoD <danielcolloto@protonmail.com>
2024-04-02 17:40:29 +0100

    Remade cards in order to be more inline with expectations

--- src/components/GitCard.jsx
+++ src/components/GitCard.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import './css/GitCard.css';
-import logo from '../collotod_remix.png';
+import logo from '../collotod_remix-removedbg.png';
 
 function GitCard({ repo }) {
     return (
         <div className="gitCard">
-            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
-                <div className="header">
-                    <img src={logo || "https://via.placeholder.com/250"} alt="Repository Logo" />
-                    <h3>{repo.full_name.split("/").slice(1).join("/")}</h3>
-                </div>
-            </a>
-                <div className="details">
-                    <h3>{repo.description || "No description available"}</h3>
-                </div>
-                <div className="stats">
-                    <div className="stargazer">
+            <div className="cardHeader">
+                <img src={logo || "https://via.placeholder.com/250"} alt="Repository Logo" />
+                <h3>{repo.full_name.split("/").slice(1).join("/")}</h3>
+            </div>
+            <div className="details">
+                <h3>{repo.description || "No description available"}</h3>
+                <div className="statistics">
+                    <div className="starCounter">
                         <h3>{repo.stargazers_count}</h3>
-                        <h3>Stargazers</h3>
+                        <h3>STARGAZERS</h3>
                     </div>
-                    <div className="stargazer">
+                    <div className="watchCounter">
                         <h3>{repo.watchers_count}</h3>
-                        <h3>Watchers</h3>
+                        <h3>WATCHERS</h3>
                     </div>
-                </div> 
+                </div>
+            </div>
         </div>
     );
 }
